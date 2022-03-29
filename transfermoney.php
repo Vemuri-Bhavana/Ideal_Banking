2a7b991652b383dd25314c9dfe927b92a5560dc3
Vemuri Bhavana <82796465+Vemuri-Bhavana@users.noreply.github.com>
2022-03-29 14:14:31 +0530

    Update transfermoney.php

--- transfermoney.php
+++ transfermoney.php
@@ -53,10 +53,10 @@
                         <option>   </option>
                         <p><?php
     
-                          $host = "ec2-35-169-188-58.compute-1.amazonaws.com";
-$user = "dpnmurbcrzfqiv";
-$pass = "57c218b8f114296469c8ebc02e35e98e306a9c5a615fd10660cd0ebc917a9dc5";
-$db = "d8upqskpp51oj3";
+                          $host = "ec2-3-209-61-239.compute-1.amazonaws.com";
+$user = "znymfvyrbmpapp";
+$pass = "fd8888af32ed35e7e25cd7c11760a8ab4eb6ee10e8abb5ada89878db38ac264b";
+$db = "d1mrogv95i40m8";
                           
                           $con = pg_connect("host=$host dbname=$db user=$user password=$pass") or die ("Could not connect to Server\n");
                           
